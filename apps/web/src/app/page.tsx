2337a003756037bd2ade6fbb826cc3dea411fc0d
edimilson braz <edimilson.gt8@gmail.com>
2023-11-22 08:13:21 -0300

    :tada: First commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from 'ui'
+
+export default function Home() {
+  return (
+    <div>
+      <Card title="Hello World" href="/">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum labore, harum aut nisi nemo dolorem ut deserunt inventore maxime animi unde, minima similique nobis cupiditate voluptatem, voluptatum dolor. Recusandae, a!
+      </Card>
+    </div>
+  )
+}
