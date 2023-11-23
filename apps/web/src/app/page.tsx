3265f4e9828d8d64355d945453ed94402399c03b
edimilson braz <edimilson.gt8@gmail.com>
2023-11-23 09:21:55 -0300

    :sparkles: Add Header

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold leading-heading text-marine-400">Hello World</h1>
+      <div className="bg-gradient-to-t from-marine-600 to-marine-500 px-6 py-5 flex flex-col gap-8">
+        <img
+          src="https://github.com/edimilsonbraz.png"
+          alt=""
+          className="rounded-full w-16 h-16 self-end"
+        />
+        <span className="text-2xl font-bold leading-heading">Dashboard</span>
+      </div>
     </div>
   )
 }
